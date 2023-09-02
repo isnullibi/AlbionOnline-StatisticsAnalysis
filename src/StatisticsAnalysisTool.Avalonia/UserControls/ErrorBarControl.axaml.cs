c39e9e0c073e36efe4fd56f903ea08444785f525
IsNULLibi <isnullibi@gmail.com>
2023-09-02 17:27:54 +0300

    ErrorBarControl rewritten from WPF to AvaloniaUI

---
+++ src/StatisticsAnalysisTool.Avalonia/UserControls/ErrorBarControl.axaml.cs
@@ -0,0 +1,41 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Projektanker.Icons.Avalonia;
+
+namespace StatisticsAnalysisTool.Avalonia.UserControls;
+
+public partial class ErrorBarControl : UserControl
+{
+    public static readonly StyledProperty<string?> ErrorTextProperty =
+        AvaloniaProperty.Register<ErrorBarControl, string?>(nameof(ErrorText));
+    
+    public string? ErrorText
+    {
+        get => GetValue(ErrorTextProperty);
+        set => SetValue(ErrorTextProperty, value);
+    }
+
+    public ErrorBarControl()
+    {
+        ErrorTextProperty.Changed.Subscribe(ErrorTextPropertyChanged);
+        InitializeComponent();
+    }
+
+    private void ErrorTextPropertyChanged(AvaloniaPropertyChangedEventArgs e)
+    {
+        if (e.NewValue?.ToString() != string.Empty) IsVisible = true;
+        else IsVisible = false;
+        //if (e.NewValue?.ToString() == string.Empty) IsVisible = false;
+        //else
+        //{
+        //    errorLabel.Content = e.NewValue.ToString();
+        //    IsVisible = true;
+        //}
+    }
+
+    private void BtnErrorBar_Click(object sender, RoutedEventArgs e)
+    {
+        ErrorText = string.Empty;
+    }
+}

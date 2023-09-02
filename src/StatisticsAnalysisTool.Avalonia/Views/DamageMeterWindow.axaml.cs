f5a9043dba02bf2d4ef5715d2bd383cb0ce5ee25
IsNULLibi <isnullibi@gmail.com>
2023-09-02 17:46:44 +0300

    DamageMeterWindow partially rewritten

---
+++ src/StatisticsAnalysisTool.Avalonia/Views/DamageMeterWindow.axaml.cs
@@ -0,0 +1,54 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace StatisticsAnalysisTool.Avalonia.Views;
+
+public partial class DamageMeterWindow : Window
+{
+    public DamageMeterWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+    private void MaximizedButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+            MaximizeButton.IsVisible = true;
+            RestoreButton.IsVisible = false;
+        }
+        else
+        {
+            WindowState = WindowState.Maximized;
+            MaximizeButton.IsVisible = false;
+            RestoreButton.IsVisible = true;
+        }
+    }
+
+    private void Grid_DoubleTapped(object sender, TappedEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized) WindowState = WindowState.Normal;
+        else WindowState = WindowState.Maximized;
+    }
+
+    private void Hotbar_Holding(object sender, PointerPressedEventArgs e)
+    {
+        try
+        {
+            BeginMoveDrag(e);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}

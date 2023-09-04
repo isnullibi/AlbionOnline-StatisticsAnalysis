5b1e9db8c7c5f84be4c2e58c53d9383c65748d5d
IsNULLibi <isnullibi@gmail.com>
2023-09-04 17:27:02 +0300

    Added SettingsControl & style adjustments

---
+++ src/StatisticsAnalysisTool.Avalonia/UserControls/SettingsControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace StatisticsAnalysisTool.Avalonia.UserControls;
+
+public partial class SettingsControl : UserControl
+{
+    public SettingsControl()
+    {
+        InitializeComponent();
+    }
+}
